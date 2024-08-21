309969c776623f4f42a0059d771b8f6bd5effc1e
BrunoPorfidio <thewarriours7@gmail.com>
2024-08-21 20:28:19 -0300

    Feature. Add Main Page.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { MainPageComponent } from './components/main-page/main-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: "main", component: MainPageComponent}
+];
