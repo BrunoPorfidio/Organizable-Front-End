7de719e82641d401b717e4685c440963e2d428ee
BrunoPorfidio <thewarriours7@gmail.com>
2024-08-22 18:33:23 -0300

    Feature. Add About Section.

---
+++ src/app/components/main-about/main-about.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-about',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-about.component.html',
+  styleUrl: './main-about.component.css'
+})
+export class MainAboutComponent {
+
+}
