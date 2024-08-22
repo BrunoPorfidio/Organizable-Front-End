8f1e70a8918ce0a05c44cbb6b18793a9a24c6b9f
BrunoPorfidio <thewarriours7@gmail.com>
2024-08-22 18:32:55 -0300

    Feature. Add About Route.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { MainAboutComponent } from './components/main-about/main-about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'about', component: MainAboutComponent }
+];
